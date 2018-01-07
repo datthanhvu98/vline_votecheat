8a2f86fc076e4badb7fcc0f31690129cc5d1039a
Stephen <16521524+stephenvxx@users.noreply.github.com>
2018-01-08 02:18:54 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 counter=1
-#Vote 1 million times (You can change your_count_submit)
+#Vote 1 million times
 while [ $counter -le 1000000 ]
 do
 echo $counter
-tel=`expr 00000000000 + $counter`
-first_email="your_name_that_you_want_set$counter" # Example first_email="kdkd$counter"
+tel=`expr 40000000000 + $counter`
+#echo $tel
+first_email="azzsfdfdtephenvu$counter"
 #echo $first_email
 email="${first_email}%40gmail.com"
-vapp_count=`expr 00000 + $counter`
-vapp="M${vapp_count}B" # You can modify First Letter and last letter M-> Random_letter and B -> Random_letter 
-cmnd=`expr 000000000 + $counter` # CMND 9 digits, you can choose CMND 9-12 digits number -> 000000000000 
+vapp_count=`printf "%05d\n" $counter`
+vapp="O${vapp_count}Q"
+#echo $vapp
+cmnd=`expr 400000000 + $counter`
+echo $cmnd
 #if [[ $cmnd -gt 0000000000000 ]]; then
 #	echo $cmnd 
 #	# Reset CMND
 #	cmnd=000000000
 #fi
-data="vote_data=fullname=Stephen+V&tel=${tel}&email=${email}&vapp=${vapp}&cmnd=${cmnd}&vs1=02&vs2=07&vs3=11&vs4=15&vs5=19&vs6=23&vs7=27"
+data="vote_data=fullname=Stephen+V&tel=${tel}&email=${email}&vapp=${vapp}&cmnd=${cmnd}&vs1=01&vs2=05&vs3=12&vs4=14&vs5=17&vs6=24&vs7=27"
 curl -X POST \
   https://voting.saostar.vn/vline/save_vote \
   -H 'cache-control: no-cache' \
