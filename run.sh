c729d504ac618fa4faf1fcc50f373a94a6a5bc04
Stephen <16521524+stephenvxx@users.noreply.github.com>
2018-01-08 03:18:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 counter=1
-#Vote 1 million times
+#Vote 1 million times (You can change your_count_submit)
 while [ $counter -le 1000000 ]
 do
 echo $counter
 tel=`expr 40000000000 + $counter`
 #echo $tel
-first_email="azzsfdfdtephenvu$counter"
+first_email="your_name_that_you_want_set$counter" # Example first_email="kdkd$counter" 
 #echo $first_email
 email="${first_email}%40gmail.com"
 vapp_count=`printf "%05d\n" $counter`
-vapp="O${vapp_count}Q"
+vapp="O${vapp_count}Q" # You can modify First Letter and last letter M-> Random_letter and B -> Random_letter 
 #echo $vapp
-cmnd=`expr 400000000 + $counter`
+cmnd=`expr 400000000 + $counter` # CMND 9 digits, you can choose CMND 9-12 digits number -> 000000000000 
 echo $cmnd
 #if [[ $cmnd -gt 0000000000000 ]]; then
 #	echo $cmnd 
